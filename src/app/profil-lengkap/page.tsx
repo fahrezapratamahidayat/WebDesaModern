72b675238b331f9d868987910d937e55cf80ce03
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-30 13:06:48 +0700

    adding profile lengkap

---
+++ src/app/profil-lengkap/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/headers/admin-header";
+import MainLayout from "@/components/layouts/main-layout";
+import ProfilLengkapSection from "@/components/sections/profil-lengkap";
+import React from "react";
+
+export default function ProfilLengkapPage() {
+  return (
+    <MainLayout>
+      <Header />
+      <ProfilLengkapSection />
+    </MainLayout>
+  );
+}
