facca40567af9d4c6cbf8761eb8b782d185d7db8
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-30 13:06:39 +0700

    adding new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,14 @@ import {
 import MainLayout from "@/components/layouts/main-layout";
 import HeroSection from "@/components/sections/hero-section";
 import Header from "@/components/headers/admin-header";
+import AboutSection from "@/components/sections/about-section";
+import GallerySection from "@/components/sections/gallery-section";
+import UmkmSection from "@/components/sections/umkm-section";
+import KegiatanSection from "@/components/sections/kegiatan-section";
+import ArtikelDesaSection from "@/components/sections/artikel-section";
+import KontakDesaSection from "@/components/sections/kontak-section";
+import FooterSection from "@/components/sections/footer-section";
+import LayananDesaSection from "@/components/sections/layanan-section";
 
 export default function Home() {
   return (
@@ -17,6 +25,14 @@ export default function Home() {
     <MainLayout>
       <Header/>
       <HeroSection />
+      <AboutSection />
+      <LayananDesaSection />
+      <GallerySection />
+      <UmkmSection />
+      <KegiatanSection />
+      <ArtikelDesaSection />
+      <KontakDesaSection />
+      <FooterSection />
     </MainLayout>
     </>
   );
