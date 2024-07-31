3634a8447468279f4400214d43e819a3a83f2bc5
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-31 21:38:07 +0700

    added kegiatan admin

---
+++ src/app/admin/kegiatan/page.tsx
@@ -0,0 +1,10 @@
+import { KegiatanDesaForm } from "@/components/form/form-kegiatan";
+import AdminLayout from "@/components/layouts/admin-layout";
+
+export default function PageKegiatan() {
+  return (
+    <AdminLayout>
+      <KegiatanDesaForm />
+    </AdminLayout>
+  );
+}
