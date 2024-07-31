2d3a2bf5bbda6634acfdd50b50fed6f045fc5d89
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-31 21:37:40 +0700

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ import {
   CarouselNext,
 } from "@/components/ui/carousel";
 import MainLayout from "@/components/layouts/main-layout";
-import HeroSection from "@/components/sections/hero-section";
+import HeroSection from "@/components/pages/hero-page";
 import Header from "@/components/headers/admin-header";
-import AboutSection from "@/components/sections/about-section";
-import GallerySection from "@/components/sections/gallery-section";
-import UmkmSection from "@/components/sections/umkm-section";
-import KegiatanSection from "@/components/sections/kegiatan-section";
-import ArtikelDesaSection from "@/components/sections/artikel-section";
-import KontakDesaSection from "@/components/sections/kontak-section";
-import FooterSection from "@/components/sections/footer-section";
-import LayananDesaSection from "@/components/sections/layanan-section";
+import AboutSection from "@/components/pages/about-page";
+import GallerySection from "@/components/pages/gallery-page";
+import UmkmSection from "@/components/pages/umkm-section-page";
+import KegiatanSection from "@/components/pages/kegiatan-page";
+import ArtikelDesaSection from "@/components/pages/artikel-page";
+import KontakDesaSection from "@/components/pages/kontak-page";
+import FooterSection from "@/components/pages/footer-page";
+import LayananDesaSection from "@/components/pages/layanan-page";
 
 export default function Home() {
   return (
