cf446f7093e2616d0ded6f5f3d970ceba1fdd4ee
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-07-31 21:38:42 +0700

    added api artikel, kegiatan

---
+++ src/app/api/artikel/route.ts
@@ -0,0 +1,18 @@
+import { createArtikel, getArtikels } from "@/services/services";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+    try {
+        const body = await req.json();
+        const data = await createArtikel(body);
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error("Error handling POST request:", error);
+        return NextResponse.json({ error: "Terjadi kesalahan pada server." }, { status: 500 });
+    }
+}
+
+export async function GET(req: NextRequest, res: NextResponse) {
+    const data = await getArtikels();
+    return NextResponse.json(data);
+}
