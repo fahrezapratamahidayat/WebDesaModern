fc70e4dd9d9ef51216d615d8bb903e8ddfbb1870
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-08-05 21:58:36 +0700

    update middelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ export default auth((req: any) => {
     const isLoggedIn = !!req.auth;
 
     const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);;
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 
     if (isApiAuthRoute) {
         return null;
