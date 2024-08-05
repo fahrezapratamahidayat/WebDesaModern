11a19597454e8faf78264ebd9d0bd50b3f00a30d
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-08-05 21:59:51 +0700

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import Image from "next/image";
-import Link from "next/link";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext,
-} from "@/components/ui/carousel";
+'use client'
 import MainLayout from "@/components/layouts/main-layout";
 import HeroSection from "@/components/pages/hero-page";
 import Header from "@/components/headers/admin-header";
 import AboutSection from "@/components/pages/about-page";
 import GallerySection from "@/components/pages/gallery-page";
-import UmkmSection from "@/components/pages/umkm-section-page";
 import KegiatanSection from "@/components/pages/kegiatan-page";
 import ArtikelDesaSection from "@/components/pages/artikel-page";
 import KontakDesaSection from "@/components/pages/kontak-page";
 import FooterSection from "@/components/pages/footer-page";
 import LayananDesaSection from "@/components/pages/layanan-page";
+import UmkmSection from "@/components/pages/umkm-section-page";
 
 export default function Home() {
   return (
