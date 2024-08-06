ee9e9fdf16987328bd649c9a11939519a0d5f6b9
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-08-06 20:14:14 +0700

    delete databases

--- src/lib/mysql/database.ts
+++
@@ -1,9 +0,0 @@
-// db.js
-
-import mysql from 'mysql2/promise';
-
-const db = mysql.createConnection({
-    host: 'localhost', user: 'root', password: '', database: 'e-learning'
-});
-
-export default db
