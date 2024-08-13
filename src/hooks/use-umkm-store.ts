68ee0753b25168c6500cb4ed4a4c17f11977ca99
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-08-13 09:51:49 +0700

    update usmkm store

--- src/hooks/use-umkm-store.ts
+++ src/hooks/use-umkm-store.ts
@@ -1,27 +1,30 @@
 import { GambarUMKM, UMKM } from '@prisma/client'
 import { create } from 'zustand'
 
-
 interface UMKMWithGambar extends UMKM {
     GambarUMKM: GambarUMKM[];
 }
 
 type UMKMStoreState = {
     editingUMKM: UMKMWithGambar | null;
-    formMode: 'create' | 'update';
+    deletingUMKM: UMKMWithGambar | null;
+    formMode: 'create' | 'update' | 'delete';
     isDialogOpen: boolean;
     setEditingUMKM: (umkm: UMKMWithGambar) => void;
     openCreateDialog: () => void;
+    openDeleteDialog: (umkm: UMKMWithGambar) => void;
     resetForm: () => void;
     closeDialog: () => void;
 };
 
 export const useUMKMStore = create<UMKMStoreState>((set) => ({
     editingUMKM: null,
+    deletingUMKM: null,
     formMode: 'create',
     isDialogOpen: false,
     setEditingUMKM: (umkm: UMKMWithGambar) => set({ editingUMKM: umkm, formMode: 'update', isDialogOpen: true }),
     openCreateDialog: () => set({ editingUMKM: null, formMode: 'create', isDialogOpen: true }),
+    openDeleteDialog: (umkm: UMKMWithGambar) => set({ deletingUMKM: umkm, formMode: 'delete', isDialogOpen: true }),
     resetForm: () => set({ editingUMKM: null, formMode: 'create' }),
     closeDialog: () => set({ isDialogOpen: false, editingUMKM: null, formMode: 'create' }),
 }));
