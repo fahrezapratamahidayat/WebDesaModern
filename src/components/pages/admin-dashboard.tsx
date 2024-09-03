29f3a2948ff6135e16ba37bdfc6a41f2d39d41ab
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2024-09-03 21:53:50 +0700

    add: admin page dashboard

---
+++ src/components/pages/admin-dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminLayout from '../layouts/admin-layout'
+
+export default function AdminDashboardPage() {
+  return (
+    <AdminLayout>
+        <div>AdminDashboardPage</div>
+    </AdminLayout>
+  )
+}
